c31fe9ef9559f690eac29062e11d9f9c33b87bd1
Don Coleman <dcoleman@chariotsolutions.com>
2015-04-05 18:04:38 -0400

    Initial Commit

---
+++ iBeacon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iBeacon
+//
+//  Created by Don Coleman on 4/5/15.
+//  Copyright (c) 2015 Don Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iBeacon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iBeacon
+//
+//  Created by Don Coleman on 4/5/15.
+//  Copyright (c) 2015 Don Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
